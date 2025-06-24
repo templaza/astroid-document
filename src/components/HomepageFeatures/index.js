8b024aab433e790323f1e18c358150aa43574840
Sonny Le <sonlv@templaza.com>
2025-06-24 17:47:14 +0700

    Finish documentation

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -3,48 +3,72 @@ import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 
 const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+    {
+        title: 'Getting Started',
+        link: '/category/get-started',
+        description: (
+            <>
+                Learn about the most important Astroid concepts through step-by-step guides. Highly recommended to go through everything from the start to the end.
+            </>
+        ),
+    },
+    {
+        title: 'Widgets',
+        link: '/category/widgets',
+        description: (
+            <>
+                Widgets are the building blocks of Astroid's user interface. They allow you to create dynamic and interactive components that enhance the user experience.
+            </>
+        ),
+    },
+    {
+        title: 'Extensions',
+        link: '/category/extensions',
+        description: (
+            <>
+                The Extensions section of the Astroid Framework documentation provides comprehensive information on the various extensions available to enhance your website's functionality.
+            </>
+        ),
+    },
+    {
+        title: 'Layout Builder',
+        link: '/category/layout-builder',
+        description: (
+            <>
+                Layout Builder is a powerful tool for creating custom layouts in Astroid Framework. It allows you to design complex page structures using a drag-and-drop interface, making it easy to build responsive and visually appealing websites without writing code.
+            </>
+        ),
+    },
+    {
+        title: 'Styling',
+        link: '/category/styling',
+        description: (
+            <>
+                The Style System is responsible for dynamic CSS generation, theming, and styling management within the Astroid Framework.
+            </>
+        ),
+    },
+    {
+        title: 'Developer Guilds',
+        link: '/category/developer-guilds',
+        description: (
+            <>
+                The purpose of this guide is to give developers an up to date guide showing best practice in writing and maintaining Astroid Widgets or modify Astroid.
+            </>
+        ),
+    },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({link, title, description}) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
+    <div className={clsx('col col--4 margin-bottom--lg')}>
+        <div className="card padding--lg feature-card">
+            <div className="text--left">
+                <Heading as="h3">{title}</Heading>
+                <p>{description}</p>
+            </div>
+            <a href={link} className="button button--secondary button--lg">Read the Docs</a>
+        </div>
     </div>
   );
 }

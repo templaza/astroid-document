10d76f447d8b97464f90f5ca07f5763e21aa5f00
Sonny Le <sonlv@templaza.com>
2025-06-25 12:02:19 +0700

    Update favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Astroid Framework',
   tagline: 'Powerful framework for designers and developers to create responsive, fast & robust Joomla based websites and templates.',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {

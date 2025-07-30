ec6ee2a5ac0b9ba4fc46cbb1e6aea0eb0b71e311
Sonny Le <sonlv@templaza.com>
2025-07-30 14:47:37 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,6 +64,11 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          filename: 'sitemap.xml',
+        },
       }),
     ],
   ],
